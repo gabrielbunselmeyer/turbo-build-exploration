52020c7416b47a9a47934ed9af8145e533907223
Gabriel Bunselmeyer <gabrielbunselmeyer@gmail.com>
2022-11-03 18:07:03 -0300

    Cleanup

--- build/packages/formatter/src/formatter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.format = void 0;
-function format(name, age) {
-    return "name: " + name + " - " + "age: " + age;
-}
-exports.format = format;
--- build/packages/math/src/add-numbers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addNumbers = void 0;
-function addNumbers(numA, numB) {
-    return numA + numB;
-}
-exports.addNumbers = addNumbers;
